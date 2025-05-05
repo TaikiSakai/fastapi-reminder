6f34b5efd0ffa6cef0823a99cbb47e2ce3685849
Taiki <s.taiki0301@gmail.com>
2025-05-05 15:34:29 +0000

    fix: transformed functional base handler

--- api/app/presentation/handler/user_handler.py
+++ api/app/presentation/handler/user_handler.py
@@ -15,38 +15,39 @@ from app.usecases.user.get_user_usecase import GetUserUsecase
 from app.schemas.users import UserCreateSchema, UserSchema
 
 
-class UserHandler:
-    def register_routes(self, router: APIRouter):
-        @router.get("/user/{user_id}")
-        def get_user(
-            user_id: int,
-            usecase: GetUserUsecase = Depends(get_user_usecase)
-        ) -> UserSchema:
-            user = usecase.execute(user_id=int(user_id))
+router = APIRouter()
 
-            return UserSchema.from_entity(user)
+@router.get("/user/{user_id}")
+def get_user(
+    user_id: int,
+    usecase: GetUserUsecase = Depends(get_user_usecase)
+) -> UserSchema:
+    user = usecase.execute(user_id=int(user_id))
 
-        @router.get("/users")
-        def get_users(data, usecase):
-            # Logic to retrieve all users
-            pass
-        
-        @router.post("/user")
-        def create_user(
-            data: UserCreateSchema,
-            usecase: CreateUserUsecase = Depends(get_create_user_usecase)
-        ) -> UserSchema:
-            user_name = UserName(data.user_name)
-            email = Email(data.email)
-            is_active = IsActive(True)
-            role = Role(data.role)
+    return UserSchema.from_entity(user)
 
-            user = usecase.execute(
-                email=email,
-                user_name=user_name,
-                is_active=is_active,
-                role=role
-            )
 
-            return UserSchema.from_entity(user)
+@router.get("/users")
+def get_users(data, usecase):
+    # Logic to retrieve all users
+    pass
 
+
+@router.post("/user")
+def create_user(
+    data: UserCreateSchema,
+    usecase: CreateUserUsecase = Depends(get_create_user_usecase)
+) -> UserSchema:
+    user_name = UserName(data.user_name)
+    email = Email(data.email)
+    is_active = IsActive(True)
+    role = Role(data.role)
+
+    user = usecase.execute(
+        email=email,
+        user_name=user_name,
+        is_active=is_active,
+        role=role
+    )
+
+    return UserSchema.from_entity(user)
