a6794852a01f3482ae297e38999579b0597b4a3a
Taiki <s.taiki0301@gmail.com>
2025-08-23 06:59:29 +0000

    feat: add env variables

--- api/app/config.py
+++ api/app/config.py
@@ -3,6 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     db_url: str
+    aws_endpoint: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
 
     model_config = SettingsConfigDict(env_file='.env.dev')
 
